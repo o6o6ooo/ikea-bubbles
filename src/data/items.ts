009d74f89acffd7fbd6e213cf9e62e9c40597312
Sakura Wallace <066sakura@gmail.com>
2026-02-25 00:33:08 +0000

    feat: add initial IKEA item collection (100+ items)

--- src/data/items.ts
+++ src/data/items.ts
@@ -18,4 +18,175 @@ export const items: IkeaItem[] = [
     name: "Vinterfint",
     description: "tree ornament",
   },
+  {
+    id: "almtjärn_round_bathmat_blue",
+    name: "Almtjärn",
+    description: "",
+  },
+  {
+    id: "bakglad_brush",
+    name: "Bakglad",
+    description: "",
+  },
+  {
+    id: "bakglad_spatula",
+    name: "Bakglad",
+    description: "",
+  },
+  {
+    id: "bevara_mixed_colours",
+    name: "Bevara",
+    description: "",
+  },
+  {
+    id: "billy_bookcase_80x28x202",
+    name: "Billy",
+    description: "",
+  },
+  {
+    id: "billy_bookcase_with_door_40x41x202",
+    name: "Billy",
+    description: "",
+  },
+  {
+    id: "bissa_white_2_compartments",
+    name: "Bissa",
+    description: "",
+  },
+  {
+    id: "bumerang_hanger_natural",
+    name: "Bumerang",
+    description: "",
+  },
+  {
+    id: "dimpa_bag_trasparent",
+    name: "Dimpa",
+    description: "",
+  },
+  {
+    id: "drömbild",
+    name: "Drömbild",
+    description: "",
+  },
+  {
+    id: "dvala_fitted_sheet_double",
+    name: "Dvala",
+    description: "",
+  },
+  {
+    id: "enudden_door_hooks",
+    name: "Enudden",
+    description: "",
+  },
+  {
+    id: "fantastisk_40x40_blue",
+    name: "Fantastisk",
+    description: "",
+  },
+  {
+    id: "filfisk",
+    name: "Filfisk",
+    description: "",
+  },
+  {
+    id: "fjädrar",
+    name: "Fjädrar",
+    description: "",
+  },
+  {
+    id: "flodnejonöga",
+    name: "Flodnejonöga",
+    description: "",
+  },
+  {
+    id: "fniss_white",
+    name: "Fniss",
+    description: "",
+  },
+  {
+    id: "fulländad_pasta_server",
+    name: "Fulländad",
+    description: "",
+  },
+  {
+    id: "fulländad_soup_ladle",
+    name: "Fulländad",
+    description: "",
+  },
+  {
+    id: "gemak",
+    name: "Gemak",
+    description: "",
+  },
+  {
+    id: "glis_blue_yellow",
+    name: "Glis",
+    description: "",
+  },
+  {
+    id: "gullsudare_shade_45cm",
+    name: "Gullsudare",
+    description: "",
+  },
+  {
+    id: "hållbar_22l",
+    name: "Hållbar",
+    description: "",
+  },
+  {
+    id: "himmelsby_frame_13x18",
+    name: "Himmelsby",
+    description: "",
+  },
+  {
+    id: "högön_parasol_grey",
+    name: "Högön",
+    description: "",
+  },
+  {
+    id: "huvudroll_meatballs",
+    name: "Huvudroll",
+    description: "",
+  },
+  {
+    id: "idealisk_strainer_20cm",
+    name: "Idealisk",
+    description: "",
+  },
+  {
+    id: "ikea365_carafe_with_stopper",
+    name: "Ikea365",
+    description: "",
+  },
+  {
+    id: "ikea365_egg_cup",
+    name: "Ikea365",
+    description: "",
+  },
+  {
+    id: "ikea365_frying_pan_stainless_28cm",
+    name: "Ikea365",
+    description: "",
+  },
+  {
+    id: "ikea365_glass_container_round_400ml",
+    name: "Ikea365",
+    description: "",
+  },
+  {
+    id: "ikea365_glass_container_round_600ml",
+    name: "Ikea365",
+    description: "",
+  },
+  {
+    id: "ikea365_glass_lid_round",
+    name: "Ikea365",
+    description: "",
+  },
+  {
+    id: "ikea365_saucepan_1l",
+    name: "Ikea365",
+    description: "",
+  },
+
 ];
